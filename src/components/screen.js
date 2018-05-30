4af9fd3a1c9bfef0a96f4609763fc714bd28544c
ersul4ik <ersul4ik@mail.ru>
2018-05-31 00:38:39 +0600

    created beginer calcuator, this code will a lot of refactoring / min style

---
+++ src/components/screen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ScreenRow from './screenRow';
+import PropTypes from 'prop-types';
+
+
+const Screen = (props) => {
+  return (
+    <div className="screen">
+      <ScreenRow value={props.question}/>
+      <ScreenRow value={props.answer}/>
+    </div>
+  );
+};
+
+Screen.propTypes = {
+  question: PropTypes.string.isRequired,
+  answer: PropTypes.string.isRequired
+};
+
+export default Screen;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Frame from './components/frame';
+
+ReactDOM.render(<Frame />, document.getElementById('root'));
